db652b159d4090c12d63617d6451a21491e4b124
Leandro <thesquallrock@gmail.com>
2021-02-16 15:01:43 +0100

    header-jumbo-section1

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var app = new Vue ({
+  el: "#root",
+  data: {
+    imgPath: 'img/',
+    features: [
+      {
+        img: 'people',
+        imgExt: '.png',
+        title: 'Human Capital',
+        p: 'Humanizing business: Harness the power of technology to improve the way people work.'
+      },
+      {
+        img: 'pie-chart',
+        imgExt: '.png',
+        title: 'Core Business',
+        p: 'It takes innovative approaches to transform, modernize, and run existing platforms.'
+      },
+      {
+        img: 'speedometer',
+        imgExt: '.png',
+        title: 'Performance',
+        p: 'Achieving maximum impact and value from investments in finance and supply chain.'
+      }
+    ]
+  },
+  methods: {
+
+  }
+});
